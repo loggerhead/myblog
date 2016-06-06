62f2d411b69830a6b71d2e8f0715933f4cc502c8
loggerhead <lloggerhead@gmail.com>
2016-06-06 20:21:47 +0800

    init

---
+++ README.md
@@ -0,0 +1,34 @@
+[![Travis Build Status](https://travis-ci.org/loggerhead/myblog.svg?branch=master)](https://travis-ci.org/loggerhead/myblog)
+
+#服务器
+##下载配置
+```bash
+mkdir -p /var/www/blog/cert
+git clone https://git.coding.net/loggerhead/myblog-nginx.git /etc/nginx
+```
+
+使用 `scp` 命令将证书放到服务器的 `/var/www/blog/cert`
+
+##运行
+```bash
+docker run -d -p 80:80 -p 443:443 -p 54321:54321 -v /etc/nginx:/etc/nginx -v /var/www/blog/cert:/var/www/blog/cert loggerhead/myblog
+```
+
+会自动从 Docker Hub 拉取，如果速度不行，使用 https://daocloud.io
+
+```bash
+docker login daocloud.io
+# 必须手动设置最新镜像为 latest
+docker pull daocloud.io/loggerhead/myblog
+```
+
+##手动构建
+```bash
+docker build --rm -t loggerhead/myblog .
+```
+
+#客户端
+```bash
+ln -s scripts/blog.sh /usr/local/bin/blog.sh
+```
+
---
+++ scripts/blog.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+WORKDIR=${BLOG_WORKDIR-$HOME/myblog}
+EDITOR=${EDITOR-vim}
+
+cd $WORKDIR
+
+if [[ $1 == "upload" ]]; then
+    make publish
+    cd output/
+
+    git add -A && git commit --amend -m 'update'
+    git push -fu origin master
+elif [[ $1 == "stop" ]] || [[ $1 == "stopserver" ]]; then
+    make stopserver
+elif [[ $1 == "make" ]]; then
+    make DEBUG=1 html
+elif [[ $1 == "open" ]]; then
+    open $WORKDIR
+elif [[ $1 == "diff" ]]; then
+    git diff --color
+elif [[ $1 == "self" ]]; then
+    $EDITOR $0
+elif [[ $1 == "dir" ]]; then
+    echo -n $WORKDIR
+elif [[ -z $1 ]]; then
+    make html
+    make devserver
+else
+    echo "$0 [OPTIONS]"
+    echo -e "\t          Make devserver"
+    echo -e "\tupload    Update articles"
+    echo -e "\tstop      Stop devserver"
+    echo -e "\tmake      Make html"
+    echo -e "\topen      Open blog directory"
+    echo -e "\tdiff      Git diff my theme"
+    echo -e "\tself      Edit this script by $EDITOR"
+    echo -e "\tdir       Print work directory"
+fi
