6406b26a228645cf8518b35aa1e140a6e32c5ffb
loggerhead <lloggerhead@gmail.com>
2017-01-19 20:24:34 +0800

    update update_blog.sh

--- update-site/update_blog.sh
+++ update-site/update_blog.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-function update_at {
-    cd $1
-    git fetch origin
-    if [[ $? != 0 ]]; then
-        echo "fetch failed"
-        exit 1
-    fi
-    git reset --hard origin/master
-    if [[ $? != 0 ]]; then
-        echo "reset failed"
-        exit 1
-    fi
-}
 
-update_at /var/www/blog/output
-update_at /etc/nginx
+set -e
+
+cd /var/www/blog/output
+git fetch origin
+git reset --hard origin/master
