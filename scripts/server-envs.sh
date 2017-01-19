174a64ac4f01661c4ea2c6edb35101d91a4877dc
loggerhead <lloggerhead@gmail.com>
2017-01-19 20:27:14 +0800

    add comments in server-envs.sh

--- scripts/server-envs.sh
+++ scripts/server-envs.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+# any HTTP GET or POST on this URL will trigger update
+export BLOG_UPDATE_URL="/coding/push"
+
+# do NOT modify below environment variables
 # the directory of HTTPS certificate
 export CERT_DIR=/var/www/blog/cert
 export NGX_LOG_DIR=/var/log/nginx
 export NGX_RUN_DIR=/var/run/nginx
 export NGX_CACHE_DIR=/var/cache/nginx
-# any HTTP GET or POST on this URL will trigger update
-export BLOG_UPDATE_URL="/coding/push"
